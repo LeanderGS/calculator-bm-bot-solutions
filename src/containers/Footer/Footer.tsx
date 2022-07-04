b0e8981a2cf46eb86deb1044248a36da95cf8469
root <Leandergs1234@gmail.com>
2022-07-03 22:35:52 -0400

    Add repository link

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return(
         <footer>
             <div>
-                <a href="#">Repositorio en GitHub.</a>
+                <a href="https://github.com/LeanderGS/calculator-bm-bot-solutions" target="_blank" rel="noreferrer">Repositorio en GitHub.</a>
             </div>
         </footer>
     );
